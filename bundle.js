c08777ec17f27fc1fb4854d8ae18d61998444a4b
Qiumin Yin <mqyin@g.ucla.edu>
2017-12-06 15:52:26 -0800

    start collision mapping

--- bundle.js
+++ bundle.js
@@ -107,16 +107,16 @@ class Map {
     this.imageX = 0;
     this.imageY = 0;
 
-    this.tankX = 10;
-    this.tankY = 10;
+    this.tankX = 0;
+    this.tankY = 0;
 
     var imageWidth = imageObj.width;
     var imageHeight = imageObj.height;
 
     context.drawImage(imageObj, this.imageX, this.imageY);
 
-    var imageData = context.getImageData(this.imageX, this.imageY, imageWidth, imageHeight);
-    this.terrainMap = imageData.data;
+    this.terrainMap = context.getImageData(this.imageX, this.imageY, imageWidth, imageHeight);
+    // this.terrainMap = imageData.data;
 
     this.drawTank();
     // this.frame();
@@ -162,8 +162,10 @@ class Map {
     }
 
 
-    tankContext.putImageData(imageData,20,20);
+    tankContext.putImageData(imageData,this.tankX,this.tankY);
     // tankContext.drawImage(tankCanvas, 10, 10);
+
+    // console.log(this.collisionTest(this.terrainMap, tankMap));
   }
 
   // frame() {
@@ -176,12 +178,35 @@ class Map {
   //   //falling
   //
   // }
-  //
-  // collisionTest(smallerObj, biggerObj) {
-  //   for (let i = 0; i < smallerObj.length; i++) {
-  //     if biggerObj
-  //   }
-  // }
+
+  collisionTest(smallerObj, biggerObj) {
+
+    for (let i = 0; i < smallerObj.height; i++) {
+      for (let j = 0; j < smallerObj.width; j++) {
+
+      }
+    }
+
+    for (let i = 0; i < smallerObj.data.length; i++) {
+
+    }
+
+  }
+
+  getPixel(x,y,map){
+    if(x < 0 || y < 0 || x > map.width || y > map.height) return;
+    let r = (y * map.width + x) * 4;
+		let g = (y * map.width + x) * 4 + 1;
+		let b = (y * map.width + x) * 4 + 2;
+		let a = (y * map.width + x) * 4 + 3;
+
+    if (map.data[r]===0 && map.data[g]===0 && map.data[b]===0 && map.data[a]===0) {
+      return true;
+    }
+    return false;
+  }
+
+
 
 }
 
