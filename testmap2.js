b92b1ab1d51f5764e2b799def230912be887f753
Qiumin Yin <mqyin@g.ucla.edu>
2017-12-07 09:29:04 -0800

    delete comments

--- testmap2.js
+++ testmap2.js
@@ -1,6 +1,5 @@
 class Map {
-  // const imageX = 0;
-  // const imageY = 0;
+
   constructor(imageObj){
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
@@ -21,7 +20,6 @@ class Map {
 
     this.terrainMap = context.getImageData(0, 0, imageWidth, imageHeight);
 
-    // this.terrainMap = imageData.data;
     document.onkeydown = this.keyDown.bind(this);
 		document.onkeyup = this.keyUp.bind(this);
 
@@ -48,22 +46,19 @@ class Map {
 
 
     this.tankContext.putImageData(this.tankMap,this.tankX,this.tankY);
-    // this.tankContext.drawImage(tankCanvas, 10, 10);
 
-    // console.log(this.collisionTest(this.tankMap, this.terrainMap));
-    // this.frame();
 
     //PIXEL SAMPLING
-    let x = 100;
-    let y = 110;
-    let r = (y * this.terrainMap.width + x) * 4;
-		let g = (y * this.terrainMap.width + x) * 4 + 1;
-		let b = (y * this.terrainMap.width + x) * 4 + 2;
-		let a = (y * this.terrainMap.width + x) * 4 + 3;
-    console.log(this.terrainMap.data[r]);
-    console.log(this.terrainMap.data[g]);
-    console.log(this.terrainMap.data[b]);
-    console.log(this.terrainMap.data[a]);
+    // let x = 100;
+    // let y = 110;
+    // let r = (y * this.terrainMap.width + x) * 4;
+		// let g = (y * this.terrainMap.width + x) * 4 + 1;
+		// let b = (y * this.terrainMap.width + x) * 4 + 2;
+		// let a = (y * this.terrainMap.width + x) * 4 + 3;
+    // console.log(this.terrainMap.data[r]);
+    // console.log(this.terrainMap.data[g]);
+    // console.log(this.terrainMap.data[b]);
+    // console.log(this.terrainMap.data[a]);
   }
 
   drawTank() {
@@ -135,12 +130,8 @@ class Map {
   			this.tankY = newY;
   		}
     }
-    // this.tankContext.clearRect(this.tankX, this.tankY, this.tankMap.width, this.tankMap.height);
-    // this.tankContext.clearRect(0, 0, 578, 400);
-    //
-    // this.tankContext.putImageData(this.tankMap,this.tankX,this.tankY);
+
     this.drawTank();
-    // console.log(test);
   }
 
   collisionTest(x, y ,smallerObj, biggerObj) {
@@ -164,27 +155,11 @@ class Map {
     if (map.data[r]===255 && map.data[g]===255 && map.data[b]===255) {
       return false;
     }
-    // if (map.data[r]===85 && map.data[g]===142 && map.data[b]===213) {
-    //   return true;
-    // }
 
     return true;
-    // return false;
   }
 
-  // getPixel(x,y,map){
-  //   if(x < 0 || y < 0 || x > map.width || y > map.height) return;
-  //   let r = (y * map.width + x) * 4;
-  //   let g = (y * map.width + x) * 4 + 1;
-  //   let b = (y * map.width + x) * 4 + 2;
-  //   let a = (y * map.width + x) * 4 + 3;
 
-  //   // if (map.data[r]===85 && map.data[g]===142 && map.data[b]===213) {
-  //   //   return true;
-  //   // }
-  //
-  //   // return false;
-  // }
 
   keyDown() {
     let KeyID = event.keyCode;
